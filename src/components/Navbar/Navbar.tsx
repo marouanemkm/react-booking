f5b0215e4dc0e28d120fa846ab7f3067c3aac7c9
Marouane <m.makroum@azapp.fr>
2022-03-03 09:26:39 +0100

    Mise en place des reducers avec typescript

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -11,7 +11,6 @@ export default function Navbar() {
     return (
         <div className='navbar'>
             <div className='left-header'>test</div>
-            <div className='middle-header'>test</div>
             <div className='right-header'><FontAwesomeIcon icon={faCartShopping} /></div>
         </div>
     )
